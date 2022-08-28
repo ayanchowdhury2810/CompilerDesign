9c3cfebc60b7f2cdd2592320f6f3c5130492934a
Ayan Chowdhury <ayan123chowdhury@gmail.com>
2022-08-28 11:03:01 +0530

    Pushed

---
+++ bubbleSort.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define max 1000
+
+void swap(int * xp, int * yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void bubbleSort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++)
+            if(arr[j]>arr[j+1])
+                swap(&arr[j], &arr[j+1]);
+    }
+}
+
+void printArray(int arr[], int size){   
+    for(int i=0; i<size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main()
+{
+    int size;
+    printf("Enter size: ");
+    scanf("%d", &size);
+    
+    int arr[size];
+    
+    printf("Enter %d elements in the array: ", size);
+    for(int i=0; i<size; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
