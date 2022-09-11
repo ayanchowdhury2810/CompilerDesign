074762246cd6a3dadddf86d455a901a1f6527509
Ayan Chowdhury <ayan123chowdhury@gmail.com>
2022-09-11 14:09:52 +0530

    Updated

--- acceptString01.py
+++ acceptString01.py
@@ -1,14 +1,8 @@
 # Construct a DFA to accept a string containing a zero followed by a one.
 
-word = "011"
+wrd = "011"
 
-for index in range(len(word)):
-    if word[index] == "1":
-        print("Not Accepted")
-        break
-    if word[index] == "0":
-        if(word[index + 1] == "1"):
-            print("Accepted")
-        else:
-            print("Not Accepted")
-            break
+if "01" in wrd:
+    print("Accepted")
+else:
+    print("Not Accepted")
