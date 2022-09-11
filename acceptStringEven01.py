1957fde93c2fec3c56a26eb6a643333415e8552c
Ayan Chowdhury <ayan123chowdhury@gmail.com>
2022-09-11 13:39:52 +0530

    Create acceptStringEven01.py

---
+++ acceptStringEven01.py
@@ -0,0 +1,17 @@
+# Construct a DFA to accept all strings containing even number of zeros and even number of ones.
+
+wrd = "00110011"
+
+count_zero = 0
+count_one = 0
+
+for letter in wrd:
+    if letter == '0':
+        count_zero += 1
+    if letter == '1':
+        count_one += 1
+
+if count_one % 2 == 0 and count_zero % 2 == 0:
+    print("Accepted")
+else:
+    print("Not Accepted")
